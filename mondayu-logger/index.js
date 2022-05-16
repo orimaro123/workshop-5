d20a250004fbe44e80980b794942513b6068f30b
Noam Neeman <noamne@monday.com>
2022-05-16 18:07:19 +0300

    init boilerplate

---
+++ mondayu-logger/index.js
@@ -0,0 +1,4 @@
+import { Console } from 'console';
+import { createWriteStream } from 'fs';
+
+export const mondayuLogger = undefined // export your monday logger here 
---
+++ weather-app/index.js
@@ -0,0 +1,23 @@
+import { syncWeather } from "@cicciosgamino/openweather-apis";
+import http from "http";
+import { mondayuLogger } from "mondayu-logger"; // import your logger
+
+const apiKey = "<<your api key>>";
+const weatherAPI = new syncWeather();
+
+const server = http.createServer(async (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  // add usage of weatherAPI here
+
+  // log the temp
+
+  res.end(
+    JSON.stringify({
+      data: 'hello mondayU',
+    })
+  );
+});
+
+server.listen(8000);
+// log that the service has started
